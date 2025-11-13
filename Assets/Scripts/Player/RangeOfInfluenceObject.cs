5af22d2c5de58ce4c37e2f09936686fb3ef2c4dd
ImAlvaroMb <alvaro207mb@gmail.com>
2025-11-13 13:02:41 +0100

    imported a few basic UI images for testing, new area of influence effect logic done + webgl project module added

--- Assets/Scripts/Player/RangeOfInfluenceObject.cs
+++ Assets/Scripts/Player/RangeOfInfluenceObject.cs
@@ -5,13 +5,15 @@ using Utilities;
 
 public class RangeOfInfluenceObject : MonoBehaviour
 {
-    public GameObject _areaSprite;
+    public GameObject[] _areaSprites;
     [SerializeField] private float influenceAreaRadius;
 
     private int _npcLayerID;
 
     [Header("Visuals")]
     [SerializeField] private float pulseDuration;
+    [SerializeField] private float minAlpha;
+    [SerializeField] private float maxAlpha;
     [SerializeField] private float minScaleFactor = 0.8f;
     [SerializeField] private float maxScaleFactor = 1.2f;
     [SerializeField] private float rotationSpeed = 90f;
@@ -24,16 +26,16 @@ public class RangeOfInfluenceObject : MonoBehaviour
 
     private void Start()
     {
-        _originalScale = _areaSprite.transform.localScale;
+        //_originalScale = _areaSprites.transform.localScale;
 
-        _minScale = _originalScale * minScaleFactor;
-        _maxScale = _originalScale * maxScaleFactor;
+        //_minScale = _originalScale * minScaleFactor;
+        //_maxScale = _originalScale * maxScaleFactor;
 
         _npcLayerID = LayerMask.NameToLayer("NPC");
 
         _collider = GetComponent<CircleCollider2D>();
         InitializeCollider();
-        StartCoroutine(PulseScale());
+        StartCoroutine(PulseAlpha());
     }
 
     private void InitializeCollider()
@@ -51,10 +53,64 @@ public class RangeOfInfluenceObject : MonoBehaviour
 
     private void Update()
     {
-        _areaSprite.transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+
     }
 
     #region Area Visuals
+
+    private IEnumerator PulseAlpha()
+    {
+        _isPulsing = true;
+
+        while (_isPulsing)
+        {
+            yield return StartCoroutine(LerpAlpha(minAlpha, maxAlpha, pulseDuration));
+            yield return StartCoroutine(LerpAlpha(maxAlpha, minAlpha, pulseDuration));
+        }
+    }
+
+    private IEnumerator LerpAlpha(float startAlpha, float endAlpha, float duration)
+    {
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+            float t = timeElapsed / duration;
+            float alpha = Mathf.Lerp(startAlpha, endAlpha, t);
+
+            foreach (var obj in _areaSprites)
+            {
+                if (obj != null)
+                {
+                    var sr = obj.GetComponent<SpriteRenderer>();
+                    if (sr != null)
+                    {
+                        Color c = sr.color;
+                        c.a = alpha;
+                        sr.color = c;
+                    }
+                }
+            }
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Ensure exact end value
+        foreach (var obj in _areaSprites)
+        {
+            if (obj != null)
+            {
+                var sr = obj.GetComponent<SpriteRenderer>();
+                if (sr != null)
+                {
+                    Color c = sr.color;
+                    c.a = endAlpha;
+                    sr.color = c;
+                }
+            }
+        }
+    }
 
     private IEnumerator PulseScale()
     {
@@ -74,13 +130,13 @@ public class RangeOfInfluenceObject : MonoBehaviour
         while(timeElapsed < duration)
         {
             float t = timeElapsed / duration;
-            _areaSprite.transform.localScale = Vector3.Lerp(startScale, endScale, t);
+            //_areaSprites.transform.localScale = Vector3.Lerp(startScale, endScale, t);
 
             timeElapsed += Time.deltaTime;
             yield return null;
         }
 
-        _areaSprite.transform.localScale = endScale;
+        //_areaSprites.transform.localScale = endScale;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
